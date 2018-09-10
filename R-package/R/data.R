86ec831e8ac9d85fa2ab4e52cdd0697c85ef83cc
mamba413 <zhuj37@mail2.sysu.edu.cn>
2018-09-10 17:14:05 +0800

    add document for the data

---
+++ R-package/R/data.R
@@ -0,0 +1,22 @@
+#' @title orbit planet data
+#' @name orbit
+#' @docType data
+#' @description
+#' A orbit planet data include 9 records about the unit normals to orbital planes.
+#'
+#' @details
+#' This orbit planet data containing 9 records about the unit normals to orbital planes.
+#' The unit normals is (sin(a)sin(b), -cos(a)sin(b), cos(b)) where a is the the inclination of
+#' the orbital plane of a planet to the ecliptic and b is the angle between a fixed line in the ecliptic
+#' and the line joining the ascending node of the planet.
+#'
+#' @format
+#' \itemize{
+#' \code{nx}: the first coordinates of the unit normals to orbital planes
+#'
+#' \code{ny}: the second coordinates of the unit normals to orbital planes
+#'
+#' \code{ny}: the third coordinates of the unit normals to orbital planes
+#' }
+#'
+NULL
